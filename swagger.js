578366b2f4b9bcef4a65893c64dfc38d92667b89
Adiquatech <quadrikolawoleadisa@gmail.com>
2025-11-09 16:28:23 +0100

    remove baspath

--- swagger.js
+++ swagger.js
@@ -8,7 +8,7 @@ const doc = {
   // host: 'localhost:3000',
   host: 'contact-api-2il0.onrender.com',
   schemes: ['https'],
-  basePath: '/',
+  // basePath: '/',
 };
 
 const outputFile = './swagger_output.json';
